9a2ae65c5a1b4dec00ad762d0a1ebb2563efbac8
Molly B <mairethedark@gmail.com>
2019-06-24 14:58:44 -0400

    Add files via upload

---
+++ Shifty.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shifty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int stuff = 0;
+            int usernum;
+            Console.WriteLine("Enter a Number");
+            usernum = int.Parse(Console.ReadLine());
+
+
+            while(usernum != 0)
+
+                {
+
+                usernum = usernum >> 1;
+                ++stuff;
+                }
+
+
+
+            Console.WriteLine(usernum);
+            Console.WriteLine(stuff);
+
+
+
+            Console.Read();
+        }
+    }
+}
